53e1086ecbd7fbb81390980ad6bcbce319f31d41
Woldemar <Woldemar8888@gmail.com>
2021-02-08 13:21:53 +0300

    version 1.1

--- js/pagination.js
+++ js/pagination.js
@@ -4,10 +4,8 @@ for(let i=0; i< buttons.length; i++){
     buttons[i].addEventListener('click', paginationHandler);
 }
 
-let obj = {'page': 1};
 function paginationHandler(e){
     let elem = e.target;
-    obj.page = elem.value;
     
     for(let i = 0; i < buttons.length; i++){
         if(buttons[i] == elem){
@@ -15,25 +13,6 @@ function paginationHandler(e){
         }else{
              buttons[i].classList.remove('activ');
         }
-    }
-    console.log(obj); 
+    } 
 }
 
-function sendSettings(obj){
-    fetch("content.php", {
-            method: "POST",
-            headers: {
-                "Content-type": "text/plain"
-            },
-            body: JSON.stringify(obj) 
-        })
-//        .then(response=>response.text())
-//        .then(data=>{
-//        try{
-//            data = JSON.parse(data);
-//                 setSongs(data);
-//            }catch(e){
-//                 console.log("error");
-//            }       
-//        });
-}
